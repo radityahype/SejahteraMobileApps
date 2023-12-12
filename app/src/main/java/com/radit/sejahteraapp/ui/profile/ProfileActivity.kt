bb9ee693e019e3522fbef2c343b144f290920b9e
Raditya Hanif Yudha Prathama <raditya.hanif01@ui.ac.id>
2023-12-12 08:53:27 +0700

    init login and profile activities

---
+++ app/src/main/java/com/radit/sejahteraapp/ui/profile/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.radit.sejahteraapp.ui.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.radit.sejahteraapp.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
