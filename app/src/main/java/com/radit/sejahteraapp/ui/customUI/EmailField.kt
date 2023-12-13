748a1c392b10a9a22bc37dd2a7b6bbd8a4e18159
Raditya Hanif Yudha Prathama <raditya.hanif01@ui.ac.id>
2023-12-13 07:28:56 +0700

    feat(login): add xml for login

---
+++ app/src/main/java/com/radit/sejahteraapp/ui/customUI/EmailField.kt
@@ -0,0 +1,59 @@
+package com.radit.sejahteraapp.ui.customUI
+
+import android.content.Context
+import android.graphics.Canvas
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import androidx.appcompat.widget.AppCompatEditText
+
+class EmailField : AppCompatEditText, View.OnTouchListener {
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context,attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super (
+        context,
+        attrs,
+        defStyleAttr) {
+
+        init()
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        hint = "Email"
+        textAlignment = View.TEXT_ALIGNMENT_VIEW_START
+    }
+
+    private fun init() {
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+                // Do nothing.
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                val targetChar = '@'
+                if (!s.toString().contains(targetChar)) {
+                    setError("Format email tidak sesuai", null)
+                } else {
+                    error = null
+                }
+            }
+
+            override fun afterTextChanged(s: Editable) {
+                // Do nothing.
+            }
+        })
+    }
+
+    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+        TODO("Not yet implemented")
+    }
+}
