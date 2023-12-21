3685f7c9f36520f37aea195ef9dca4407b315c47
Raditya Hanif Yudha Prathama <raditya.hanif01@ui.ac.id>
2023-12-22 05:51:58 +0700

    feat(result-page): add layout for result page

---
+++ app/src/main/java/com/radit/sejahteraapp/ui/resultPage/ResultPageActivity.kt
@@ -0,0 +1,12 @@
+package com.radit.sejahteraapp.ui.resultPage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.radit.sejahteraapp.R
+
+class ResultPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result_page)
+    }
+}
